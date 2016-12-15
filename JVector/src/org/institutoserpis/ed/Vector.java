9084c8471bfde14d6eff4b402ed1f32344dd4ca9
carolinajg66 <carolinajustinianoguzman@gmail.com>
2016-12-15 13:46:35 +0100

    Vector indexOf y min cambiados usando foreach de java

--- JVector/src/org/institutoserpis/ed/Vector.java
+++ JVector/src/org/institutoserpis/ed/Vector.java
@@ -7,24 +7,36 @@ public class Vector {
 		System.out.println("Vector index = "+index);
 	}
 	
-	public static int indexOf(int[] v, int value ){
+	public static int indexOf(int[] v, int data ){
 		//Wirth Algoritmos y Estructura de datos.
-		int index = 0; 
-		while (index < v.length && v[index] !=value)
-			index++;
-		if (index ==v.length)
-		 return -1;
-		return index; 
+//		int index = 0; 
+//		while (index < v.length && v[index] !=data)
+//			index++;
+//		if (index ==v.length)
+//		 return -1;
+//		return index; 
+		
+		
+		int index =0;
+		for (int item :v){
+			if (item == data)
+				return index;
+			index ++;
+		}
+		return -1;
 	}
 	
 	public static int min(int[] v){
-		int min=v [0]; 
-		 for(int index=1;index<v.length;index++){
-			 if (v[index]<min)
-				 min=v[index];
-		 }
+		int min=v[0]; 
+//		 for(int index=1;index<v.length;index++){
+//			 if (v[index]<min)
+//				 min=v[index];
+//		 }
+		for (int item:v)
+			if (item<min)
+				min=item;
 		 return min;
-	}
+	} 
 }
 		
 		
