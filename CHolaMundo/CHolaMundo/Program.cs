1f02971009cf500faa37f6bad05898548357b29a
carolinajg66 <carolinajustinianoguzman@gmail.com>
2017-01-19 13:47:48 +0100

    Creado CHolaMundo(Hola Mundo en C# con monodevelop

---
+++ CHolaMundo/CHolaMundo/Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Org.InstitutoSerpis.Ed
+{
+	class HolaMundo
+	{
+		public static void Main (string[] args)
+
+		{
+			Console.Write ("Introduce tu nombre");
+			string nombre = Console.ReadLine ();
+
+			Console.WriteLine ("Hello "+ nombre);
+		}
+	}
+
+}
